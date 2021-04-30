d6facf86a04b5f0dad94c99fafa673b5017829e6
Sebastian Pawlak <sebastian.pawlak-c@wpengine.com>
2021-04-30 09:30:26 +0200

    set wrong port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const server = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 server.get('/', (req, res) => {
   console.log(process.env);
